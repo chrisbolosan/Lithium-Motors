9c4d190e8bc3c2a601710a0e74236deee4665857
chrisbolosan <cbolosan@gmail.com>
2022-01-12 15:10:52 -0600

    navbar up

--- src/LandingContainer/LandingContainer.js
+++ src/LandingContainer/LandingContainer.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Footer from "./Footer/Footer";
 import Home from "./Home/Home";
-import Navigation from "./Navigation/Navigation";
+import NavigationBar from "./Navigation/NavigationBar";
 import Product from "./Products/Products";
 
 export default function LandingContainer() {
   return (
     <div className="landing-container">
       <h1>Landing Container</h1>
-      <Navigation />
+      <NavigationBar />
       <Home />
       <Product />
       {/* <Footer /> */}
--- src/LandingContainer/Navigation/Navigation.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Navigation() {
-  return <div className="navigation-container">Lorem ipsum</div>;
-}
