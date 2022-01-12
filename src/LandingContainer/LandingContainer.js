e321fa1309dfd64ca083ebf0f6e9571feefe051c
chrisbolosan <cbolosan@gmail.com>
2022-01-12 13:10:18 -0600

    product more styling, break

--- src/LandingContainer/LandingContainer.js
+++ src/LandingContainer/LandingContainer.js
@@ -1,15 +1,17 @@
 import React from "react";
+import Footer from "./Footer/Footer";
 import Home from "./Home/Home";
 import Navigation from "./Navigation/Navigation";
 import Product from "./Products/Products";
 
 export default function LandingContainer() {
   return (
-    <div className="LandingContainer">
+    <div className="landing-container">
       <h1>Landing Container</h1>
       <Navigation />
       <Home />
       <Product />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/LandingContainer/Products/Products.js
+++ src/LandingContainer/Products/Products.js
@@ -5,28 +5,33 @@ import earbudsImg from "../../assets/Product/earbuds.jpg";
 import glassesImg from "../../assets/Product/glasses.jpg";
 import plantImg from "../../assets/Product/plant.jpg";
 import checkImg from "../../assets/Product/arrow.svg";
+import "./Products.css";
 
 export default function Product() {
   return (
     <section className="product-container">
-      <ul>
-        <li>
+      <ul className="product-parent">
+        <li className="product-details">
           <img src={barStoolImg} alt="" />
           <h3>Lorem ipsum 1</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p className="product-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
 
           <ul className="colors">
-            <li className="color blue"></li>
-            <li className="color purple"></li>
-            <li className="color pink"></li>
+            <li className="rectangle-1"></li>
+            <li className="rectangle-2"></li>
+            <li className="rectangle-3"></li>
           </ul>
 
           <button>Add to cart</button>
         </li>
-        <li>
+        <li className="product-details">
           <img src={earbudsImg} alt="" />
           <h3>Lorem ipsum 2</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p className="product-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
 
           <ul className="colors">
             <li className="color blue"></li>
@@ -36,10 +41,12 @@ export default function Product() {
 
           <button>Add to cart</button>
         </li>
-        <li>
+        <li className="product-details">
           <img src={glassesImg} alt="" />
           <h3>Lorem ipsum 3</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p className="product-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
 
           <ul className="colors">
             <li className="color blue"></li>
@@ -49,10 +56,12 @@ export default function Product() {
 
           <button>Add to cart</button>
         </li>
-        <li>
+        <li className="product-details">
           <img src={plantImg} alt="" />
           <h3>Lorem ipsum 4</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p className="product-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
 
           <ul className="colors">
             <li className="color blue">
