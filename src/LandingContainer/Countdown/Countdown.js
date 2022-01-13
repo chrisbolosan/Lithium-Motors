1108afbc4c839b4365912e64b350c4996d822c32
chrisbolosan <cbolosan@gmail.com>
2022-01-12 22:55:22 -0600

    countdown section started

--- src/LandingContainer/Countdown/Countdown.js
+++ src/LandingContainer/Countdown/Countdown.js
@@ -1,11 +1,21 @@
 import React from "react";
-
+import "./Countdown.css";
 export default function Countdown() {
   return (
-    <div>
-      <h1>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit Countdown here
-      </h1>
+    <div className="countdown-container">
+      <div className="countdown-parent">
+        <div className="countdown-details">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit{" "}
+          <div className="countdown-clock-container">
+            <div id="clockdiv">
+              Days: <span class="days"></span>
+              Hours: <span class="hours"></span>
+              Minutes: <span class="minutes"></span>
+              Seconds: <span class="seconds"></span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
