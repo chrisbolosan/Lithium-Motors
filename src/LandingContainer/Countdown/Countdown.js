aa97dc6735878e5fd2fd7d8f07d945ae822ca88c
chrisbolosan <cbolosan@gmail.com>
2022-01-12 22:39:59 -0600

    countdown component established

---
+++ src/LandingContainer/Countdown/Countdown.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Countdown() {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit Countdown here
+      </h1>
+    </div>
+  );
+}
--- src/LandingContainer/LandingContainer.js
+++ src/LandingContainer/LandingContainer.js
@@ -3,14 +3,13 @@ import Footer from "./Footer/Footer";
 import Home from "./Home/Home";
 import NavigationBar from "./Navigation/NavigationBar";
 import Product from "./Products/Products";
+import Countdown from "./Countdown/Countdown";
 import "./LandingContainer.css";
 
 export default function LandingContainer() {
   return (
     <div className="landing-container">
-      <h1>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit Countdown here
-      </h1>
+      <Countdown />
       <NavigationBar />
       <div className="picture-overlap"></div>
       <Home />
